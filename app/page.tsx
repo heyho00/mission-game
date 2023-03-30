c21a416c6eb243d44f433ae3c449315267682fc2
heyho00 <heyho9292@gmail.com>
2023-03-30 21:59:44 +0900

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,78 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import styles from "./page.module.css";
 
 export default function Home() {
+  const [missions, setMissions] = useState<{ title: string; items: string[] }>({
+    title: "",
+    items: [],
+  });
+
+  const handleCountryClick = async (country: string) => {
+    try {
+      const response = await fetch(`/api/missions?country=${country}`);
+      const data = await response.json();
+      setMissions(data);
+      toast.success(`🚀 ${country} 미션 추천 완료!`);
+    } catch (error) {
+      console.error(error);
+      toast.error(`❌ ${country} 미션 추천 실패!`);
+    }
+  };
+  console.log(missions, "미션스");
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+        <div
+          className={styles.thirteen}
+          style={{ cursor: "pointer" }}
+          onClick={() => handleCountryClick("서울")}
+        >
+          서울
+        </div>
+        <div
+          className={styles.thirteen}
+          style={{ cursor: "pointer" }}
+          onClick={() => handleCountryClick("방콕")}
+        >
+          방콕
+        </div>
+        <div
+          className={styles.thirteen}
+          style={{ cursor: "pointer" }}
+          onClick={() => handleCountryClick("영국")}
+        >
+          영국
+        </div>
+        <div
+          className={styles.thirteen}
+          style={{ cursor: "pointer" }}
+          onClick={() => handleCountryClick("괌")}
+        >
+          괌
+        </div>
+        <div
+          className={styles.thirteen}
+          style={{ cursor: "pointer" }}
+          onClick={() => handleCountryClick("일본")}
+        >
+          일본
         </div>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <ToastContainer />
+      {missions.title && (
+        <div className={styles.missions}>
+          <h2>{missions.title}</h2>
+          <ul>
+            {missions.items.map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </main>
-  )
+  );
 }
